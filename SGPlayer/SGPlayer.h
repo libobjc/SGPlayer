86d4863f694576db65a328e92ff39a34b0a1a425
Single <musicman_leehom@126.com>
2017-03-09 13:42:24 +0800

    Initial Commit

---
+++ SGPlayer/SGPlayer.h
@@ -0,0 +1,19 @@
+//
+//  SGPlayer.h
+//  SGPlayer
+//
+//  Created by Single on 2017/3/9.
+//  Copyright © 2017年 single. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SGPlayer.
+FOUNDATION_EXPORT double SGPlayerVersionNumber;
+
+//! Project version string for SGPlayer.
+FOUNDATION_EXPORT const unsigned char SGPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGPlayer/PublicHeader.h>
+
+
