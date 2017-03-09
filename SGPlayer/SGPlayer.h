d832f689ac050c9589e2ba53a3cabd102e9f918a
Single <musicman_leehom@126.com>
2017-03-09 14:07:45 +0800

    improt SGPlatform

--- SGPlayer/SGPlayer.h
+++ SGPlayer/SGPlayer.h
@@ -6,14 +6,10 @@
 //  Copyright © 2017年 single. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for SGPlayer.
 FOUNDATION_EXPORT double SGPlayerVersionNumber;
-
-//! Project version string for SGPlayer.
 FOUNDATION_EXPORT const unsigned char SGPlayerVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SGPlayer/PublicHeader.h>
-
-
+// Platform
+#impro <SGPlatform/SGPlatform.h>
