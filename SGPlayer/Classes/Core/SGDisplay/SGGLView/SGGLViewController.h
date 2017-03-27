1d4583602da640e50d2f248f5fe5affb6052436b
Single <musicman_leehom@126.com>
2017-03-27 10:47:26 +0800

    add SGGLViewController

---
+++ SGPlayer/Classes/Core/SGDisplay/SGGLView/SGGLViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGGLViewController.h
+//  SGPlayer
+//
+//  Created by Single on 2017/3/27.
+//  Copyright © 2017年 single. All rights reserved.
+//
+
+#import <SGPlatform/SGPlatform.h>
+
+@interface SGGLViewController : SGPLFGLViewController
+
+@end
---
+++ SGPlayer/Classes/Core/SGDisplay/SGGLView/SGGLViewController.m
@@ -0,0 +1,13 @@
+//
+//  SGGLViewController.m
+//  SGPlayer
+//
+//  Created by Single on 2017/3/27.
+//  Copyright © 2017年 single. All rights reserved.
+//
+
+#import "SGGLViewController.h"
+
+@implementation SGGLViewController
+
+@end
