0410daaffcf16189031ed29fbef3e94e2fa259d1
Single <musicman_leehom@126.com>
2017-03-27 16:41:31 +0800

    add SGGLTexture

---
+++ SGPlayer/Classes/Core/SGDisplay/SGGLTexture/SGGLTexture.m
@@ -0,0 +1,23 @@
+//
+//  SGGLTexture.m
+//  SGPlayer
+//
+//  Created by Single on 2017/3/27.
+//  Copyright © 2017年 single. All rights reserved.
+//
+
+#import "SGGLTexture.h"
+
+@implementation SGGLTexture
+
+- (BOOL)updateTextureWithGLFrame:(SGGLFrame *)glFrame aspect:(CGFloat *)aspect
+{
+    return NO;
+}
+
+- (void)flush
+{
+    
+}
+
+@end
