3db33520ad98fbf61e015ffad1e94e85ed63b660
Single <musicman_leehom@126.com>
2017-03-28 18:45:37 +0800

    update frame queue limit count

--- SGPlayer/Classes/Core/SGFFPlayer/SGFFmpeg/SGFFVideoDecoder.m
+++ SGPlayer/Classes/Core/SGFFPlayer/SGFFmpeg/SGFFVideoDecoder.m
@@ -238,13 +238,13 @@ static NSTimeInterval max_video_frame_sleep_full_and_pause_time_interval = 0.5;
             BOOL result = [self.videoToolBox sendPacket:packet needFlush:&needFlush];
             if (result) {
                 videoFrame = [self videoFrameFromVideoToolBox:packet];
-                self.frameQueue.minFrameCountForGet = 5;
+                self.frameQueue.minFrameCountForGet = 4;
             } else if (needFlush) {
                 [self.videoToolBox flush];
                 BOOL result2 = [self.videoToolBox sendPacket:packet needFlush:&needFlush];
                 if (result2) {
                     videoFrame = [self videoFrameFromVideoToolBox:packet];
-                    self.frameQueue.minFrameCountForGet = 5;
+                    self.frameQueue.minFrameCountForGet = 4;
                 }
             }
         } else {
