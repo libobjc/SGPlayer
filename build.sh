f01daf1877b499bb4b583562d601efa6bd147a57
Single <musicman_leehom@126.com>
2017-04-07 13:04:59 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -10,14 +10,18 @@ LIB_FFMPEG_FILE_NAME_MACOS="lib-macOS"
 LIB_FFMPEG_FILE_NAME_TVOS="lib-tvOS"
 
 do_lib_ffmpeg () {
-  LIB_FFMPEG_DOWNLOAD_URL="http://omw595ki7.bkt.clouddn.com/ffmpeg/$LIB_FFMPEG_FILE_NAME.zip"
-  echo "download lib ffmpeg..."
-  curl -o $ROOT_PATH/$LIB_FFMPEG_FILE_NAME.zip $LIB_FFMPEG_DOWNLOAD_URL
-  echo "unzip lib ffmpeg..."
-  unzip $ROOT_PATH/$LIB_FFMPEG_FILE_NAME.zip -d $ROOT_PATH/SGPlayer/Classes/Core/SGFFPlayer/ffmpeg/
-  echo "clean build file..."
-  rm -rf $ROOT_PATH/$LIB_FFMPEG_FILE_NAME.zip
-  echo "build done."
+  if [ -d $ROOT_PATH/SGPlayer/Classes/Core/SGFFPlayer/ffmpeg/$LIB_FFMPEG_FILE_NAME ]; then
+    echo "lib ffmpeg exist."
+  else
+    LIB_FFMPEG_DOWNLOAD_URL="http://omw595ki7.bkt.clouddn.com/ffmpeg/$LIB_FFMPEG_FILE_NAME.zip"
+    echo "download lib ffmpeg..."
+    curl -o $ROOT_PATH/$LIB_FFMPEG_FILE_NAME.zip $LIB_FFMPEG_DOWNLOAD_URL
+    echo "unzip lib ffmpeg..."
+    unzip $ROOT_PATH/$LIB_FFMPEG_FILE_NAME.zip -d $ROOT_PATH/SGPlayer/Classes/Core/SGFFPlayer/ffmpeg/
+    echo "clean temp file..."
+    rm -rf $ROOT_PATH/$LIB_FFMPEG_FILE_NAME.zip
+    echo "download lib ffmpeg done."
+  fi
 }
 
 do_framework_SGPlatform () {
@@ -41,16 +45,19 @@ if [ "$ARGV1" == "iOS" ]; then
   LIB_FFMPEG_FILE_NAME=$LIB_FFMPEG_FILE_NAME_IOS
   do_framework_SGPlatform
   do_lib_ffmpeg
+  echo "build iOS done."
 elif [ "$ARGV1" == "macOS" ]; then
   echo "build for macOS."
   LIB_FFMPEG_FILE_NAME=$LIB_FFMPEG_FILE_NAME_MACOS
   do_framework_SGPlatform
   do_lib_ffmpeg
+  echo "build macOS done."
 elif [ "$ARGV1" == "tvOS" ]; then
   echo "build for tvOS."
   LIB_FFMPEG_FILE_NAME=$LIB_FFMPEG_FILE_NAME_TVOS
   do_framework_SGPlatform
   do_lib_ffmpeg
+  echo "build tvOS done."
 else
   echo echo "Usage:"
   echo "  build.sh iOS"
