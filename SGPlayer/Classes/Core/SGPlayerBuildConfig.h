66a6b2d6e22915feed910bb20e74a53c6c481919
Single <libobjc@gmail.com>
2017-11-02 15:27:27 +0800

    add build config

---
+++ SGPlayer/Classes/Core/SGPlayerBuildConfig.h
@@ -0,0 +1,15 @@
+//
+//  SGPlayerBuildConfig.h
+//  SGPlayer
+//
+//  Created by Single on 2017/11/2.
+//  Copyright © 2017年 single. All rights reserved.
+//
+
+#ifndef SGPlayerBuildConfig_h
+#define SGPlayerBuildConfig_h
+
+#define SGPlayerBuildConfig_AVPlayer_Disable    NO
+#define SGPlayerBuildConfig_FFmpeg_Disable      NO
+
+#endif /* SGPlayerBuildConfig_h */
