16df450672cba95306f5d9506789d915bdab2f24
Single <libobjc@gmail.com>
2018-01-29 17:37:36 +0800

    add ASGFFSyncClock

---
+++ SGPlayer/Classes/Core/SGFFPlayer/SGFFmpeg/SGFFOutput/SGFFSyncClock.h
@@ -0,0 +1,13 @@
+//
+//  SGFFSyncClock.h
+//  SGPlayer
+//
+//  Created by Single on 2018/1/29.
+//  Copyright © 2018年 single. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SGFFSyncClock : NSObject
+
+@end
---
+++ SGPlayer/Classes/Core/SGFFPlayer/SGFFmpeg/SGFFOutput/SGFFSyncClock.m
@@ -0,0 +1,13 @@
+//
+//  SGFFSyncClock.m
+//  SGPlayer
+//
+//  Created by Single on 2018/1/29.
+//  Copyright © 2018年 single. All rights reserved.
+//
+
+#import "SGFFSyncClock.h"
+
+@implementation SGFFSyncClock
+
+@end
