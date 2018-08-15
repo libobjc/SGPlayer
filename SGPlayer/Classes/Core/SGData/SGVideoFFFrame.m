2e8bd783259694ad173f3c2f897c83563120491f
Single <libobjc@gmail.com>
2018-08-15 16:37:50 +0800

    update

--- SGPlayer/Classes/Core/SGData/SGVideoFFFrame.m
+++ SGPlayer/Classes/Core/SGData/SGVideoFFFrame.m
@@ -8,6 +8,16 @@
 
 #import "SGVideoFFFrame.h"
 
+@interface SGVideoFFFrame ()
+
+{
+    uint8_t * _resampleData[8];
+    size_t _resampleDataSize[8];
+    int _resampleLinesize[8];
+}
+
+@end
+
 @implementation SGVideoFFFrame
 
 - (instancetype)init
@@ -16,6 +26,12 @@
     {
         NSLog(@"%s", __func__);
         _coreFrame = av_frame_alloc();
+        for (int i = 0; i < 8; i++)
+        {
+            _resampleData[i] = NULL;
+            _resampleDataSize[i] = 0;
+            _resampleLinesize[i] = 0;
+        }
     }
     return self;
 }
@@ -27,6 +43,16 @@
     {
         av_frame_free(&_coreFrame);
         _coreFrame = NULL;
+    }
+    for (int i = 0; i < 8; i++)
+    {
+        _resampleLinesize[i] = 0;
+        if (_resampleData[i] != NULL && _resampleDataSize[i] > 0)
+        {
+            free(_resampleData[i]);
+            _resampleData[i] = NULL;
+            _resampleDataSize[i] = 0;
+        }
     }
 }
 
@@ -57,8 +83,57 @@
     self.packetPosition = av_frame_get_pkt_pos(self.coreFrame);
     self.packetDuration = av_frame_get_pkt_duration(self.coreFrame);
     self.packetSize = av_frame_get_pkt_size(self.coreFrame);
-    self.data = self.coreFrame->data;
-    self.linesize = self.coreFrame->linesize;
+    
+    BOOL resample = [self resampleIfNeeded];
+    self.data = resample ? _resampleData : self.coreFrame->data;
+    self.linesize = resample ? _resampleLinesize : self.coreFrame->linesize;
+}
+
+- (BOOL)resampleIfNeeded
+{
+    BOOL resample = NO;
+    int channels = 0;
+    int linesize[8] = {0};
+    int linecount[8] = {0};
+    if (self.format == AV_PIX_FMT_YUV420P)
+    {
+        channels = 3;
+        linesize[0] = self.width;
+        linesize[1] = self.width / 2;
+        linesize[2] = self.width / 2;
+        linecount[0] = self.height;
+        linecount[1] = self.height / 2;
+        linecount[2] = self.height / 2;
+    }
+    for (int i = 0; i < channels; i++)
+    {
+        if (self.coreFrame->linesize[i] > linesize[i])
+        {
+            resample = YES;
+            size_t size = linesize[i] * linecount[i] * sizeof(uint8_t);
+            if (_resampleDataSize[i] < size)
+            {
+                if (_resampleData[i] != NULL && _resampleDataSize[i] > 0)
+                {
+                    free(_resampleData[i]);
+                    _resampleData[i] = NULL;
+                    _resampleDataSize[i] = 0;
+                }
+                _resampleData[i] = malloc(size);
+                _resampleDataSize[i] = size;
+            }
+            uint8_t * dest = _resampleData[i];
+            uint8_t * src = self.coreFrame->data[i];
+            for (int j = 0; j < linecount[i]; j++)
+            {
+                memcpy(dest, src, linesize[i] * sizeof(uint8_t));
+                dest += linesize[i];
+                src += self.coreFrame->linesize[i];
+            }
+            _resampleLinesize[i] = linesize[i];
+        }
+    }
+    return resample;
 }
 
 - (void)clear
