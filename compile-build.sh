76c6865e9a4f8e444d8f83a8500b63e2d92887bf
Single <libobjc@gmail.com>
2018-11-05 16:59:55 +0800

    Update build.sh

--- compile-build.sh
+++ compile-build.sh
@@ -5,10 +5,17 @@ ARGV1=$1
 ROOT_PATH=`pwd`
 FFMPEG_VERSION="4.0.2"
 FFMPEG_PATH=$ROOT_PATH/SGPlayer/Classes/Core/SGFFmpeg/
+
 FFMPEG_LIB_FILE_NAME=""
-FFMPEG_LIB_FILE_NAME_IOS="FFmpeg-"$FFMPEG_VERSION"-lib-iOS"
-FFMPEG_LIB_FILE_NAME_MACOS="FFmpeg-"$FFMPEG_VERSION"-lib-macOS"
-FFMPEG_LIB_FILE_NAME_TVOS="FFmpeg-"$FFMPEG_VERSION"-lib-tvOS"
+FFMPEG_LIB_FILE_NAME_IOS="lib-iOS"
+FFMPEG_LIB_FILE_NAME_MACOS="lib-macOS"
+FFMPEG_LIB_FILE_NAME_TVOS="lib-tvOS"
+FFMPEG_INCLUDE_FILE_NAME="include"
+
+FFMPEG_LIB_DOWNLOAD_NAME=""
+FFMPEG_LIB_DOWNLOAD_NAME_IOS="FFmpeg-"$FFMPEG_VERSION"-lib-iOS"
+FFMPEG_LIB_DOWNLOAD_NAME_MACOS="FFmpeg-"$FFMPEG_VERSION"-lib-macOS"
+FFMPEG_LIB_DOWNLOAD_NAME_TVOS="FFmpeg-"$FFMPEG_VERSION"-lib-tvOS"
 FFMPEG_INCLUDE_FILE_NAME="FFmpeg-"$FFMPEG_VERSION"-include"
 
 do_ffmpeg_lib () {
@@ -16,7 +23,7 @@ do_ffmpeg_lib () {
   if [ -d LIB_FFMPEG_FILE_PATH ]; then
     echo "ffmpeg lib exist."
   else
-    FFMPEG_LIB_DOWNLOAD_URL="http://omw595ki7.bkt.clouddn.com/FFmpeg/$FFMPEG_LIB_FILE_NAME.zip"
+    FFMPEG_LIB_DOWNLOAD_URL="http://omw595ki7.bkt.clouddn.com/FFmpeg/$FFMPEG_LIB_DOWNLOAD_NAME.zip"
     echo "download ffmpeg lib..."
     curl -o $LIB_FFMPEG_FILE_PATH.zip $FFMPEG_LIB_DOWNLOAD_URL
     echo "unzip ffmpeg lib..."
@@ -32,7 +39,7 @@ do_ffmpeg_inlcude () {
   if [ -d INCLUDE_FFMPEG_FILE_PATH ]; then
     echo "ffmpeg include exist."
   else
-    INCLUDE_FFMPEG_DOWNLOAD_URL="http://omw595ki7.bkt.clouddn.com/FFmpeg/$FFMPEG_INCLUDE_FILE_NAME.zip"
+    INCLUDE_FFMPEG_DOWNLOAD_URL="http://omw595ki7.bkt.clouddn.com/FFmpeg/$FFMPEG_INCLUDE_DOWNLOAD_NAME.zip"
     echo "download ffmpeg include..."
     curl -o $INCLUDE_FFMPEG_FILE_PATH.zip $INCLUDE_FFMPEG_DOWNLOAD_URL
     echo "unzip ffmpeg include..."
@@ -46,18 +53,21 @@ do_ffmpeg_inlcude () {
 if [ "$ARGV1" == "iOS" ]; then
   echo "build for iOS."
   FFMPEG_LIB_FILE_NAME=$FFMPEG_LIB_FILE_NAME_IOS
+  FFMPEG_LIB_DOWNLOAD_NAME=$FFMPEG_LIB_DOWNLOAD_NAME_IOS
   do_ffmpeg_inlcude
   do_ffmpeg_lib
   echo "build iOS done."
 elif [ "$ARGV1" == "macOS" ]; then
   echo "build for macOS."
   FFMPEG_LIB_FILE_NAME=$FFMPEG_LIB_FILE_NAME_MACOS
+  FFMPEG_LIB_DOWNLOAD_NAME=$FFMPEG_LIB_DOWNLOAD_NAME_MACOS
   do_ffmpeg_inlcude
   do_ffmpeg_lib
   echo "build macOS done."
 elif [ "$ARGV1" == "tvOS" ]; then
   echo "build for tvOS."
   FFMPEG_LIB_FILE_NAME=$FFMPEG_LIB_FILE_NAME_TVOS
+  FFMPEG_LIB_DOWNLOAD_NAME=$FFMPEG_LIB_DOWNLOAD_NAME_TVOS
   do_ffmpeg_inlcude
   do_ffmpeg_lib
   echo "build tvOS done."
--- compile-clean.sh
+++ compile-clean.sh
@@ -3,7 +3,8 @@
 do_clean_ffmpeg () {
   echo "clean ffmpeg..."
   cd SGPlayer/Classes/Core/SGFFmpeg/
-  rm -rf -v `find . -name "FFmpeg-*"`
+  rm -rf -v `find . -name "include"`
+  rm -rf -v `find . -name "lib-*"`
   rm -rf -v `find . -name "__MACOSX"`
   echo "clean ffmpeg done."
 }
