3e07a0712fdb54c208970fd8758cd8c5d8b51306
Charles Wang <chaocharleswang@gmail.com>
2019-09-08 13:40:38 -0500

    Update SGVideoRenderer.m
    Fix video replacing crash in background

--- SGPlayer/Classes/Core/SGRenderer/SGVideoRenderer.m
+++ SGPlayer/Classes/Core/SGRenderer/SGVideoRenderer.m
@@ -395,7 +395,13 @@
     }
     MTLViewport viewports[2] = {};
     NSArray<SGMetalProjection *> *projections = nil;
-    id<CAMetalDrawable> drawable = [(CAMetalLayer *)self->_metalView.layer nextDrawable];
+    CAMetalLayer *layer = (CAMetalLayer *)self->_metalView.layer;
+    
+    if (layer.drawableSize.width == 0 || layer.drawableSize.height == 0) {
+        return;
+    }
+    
+    id<CAMetalDrawable> drawable = [layer nextDrawable];
     MTLSize textureSize = MTLSizeMake(width, height, 0);
     MTLSize layerSize = MTLSizeMake(drawable.texture.width, drawable.texture.height, 0);
     switch (displayMode) {
