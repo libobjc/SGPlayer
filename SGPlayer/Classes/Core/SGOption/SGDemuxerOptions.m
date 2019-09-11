d7ce8632b7792d674b4d20bce913b0da5b6737cb
Single <libobjc@gmail.com>
2019-09-11 17:55:40 +0800

    SGDemuxerOptions: Formatting.

--- SGPlayer/Classes/Core/SGOption/SGDemuxerOptions.m
+++ SGPlayer/Classes/Core/SGOption/SGDemuxerOptions.m
@@ -20,9 +20,9 @@
 - (instancetype)init
 {
     if (self = [super init]) {
-        self->_options = @{@"timeout" : @(20 * 1000 * 1000),
-                           @"reconnect" : @(1),
-                           @"user-agent" : @"SGPlayer"};
+        self->_options = @{@"reconnect" : @(1),
+                           @"user-agent" : @"SGPlayer",
+                           @"timeout" : @(20 * 1000 * 1000)};
     }
     return self;
 }
